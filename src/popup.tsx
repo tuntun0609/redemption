6de880a88d55ee08c730caba40c0c2f96d2b4bb2
tuntun <760523867@qq.com>
2023-08-02 22:41:43 +0800

    feat: init

--- src/popup.tsx
+++ src/popup.tsx
@@ -1,21 +1,21 @@
-import { useState } from "react"
+import { useState } from 'react'
 
 function IndexPopup() {
-  const [data, setData] = useState("")
+  const [data, setData] = useState('')
 
   return (
     <div
       style={{
-        display: "flex",
-        flexDirection: "column",
+        display: 'flex',
+        flexDirection: 'column',
         padding: 16
       }}>
       <h2>
         Welcome to your
         <a href="https://www.plasmo.com" target="_blank">
-          {" "}
+          {' '}
           Plasmo
-        </a>{" "}
+        </a>{' '}
         Extension!
       </h2>
       <input onChange={(e) => setData(e.target.value)} value={data} />
